972dc6f73272ff29f9feb159b254bfab4a1414bb
Poonam <Poonam@THP_Poonam>
2025-11-22 17:16:49 +0530

    First commit

---
+++ maven_jenkin/src/test/java/Demo/SampleTest.java
@@ -0,0 +1,16 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+     @Test
+	  public void case1()
+	  {
+		  
+		  System.out.println("Print Case1");
+		  
+	  }
+	
+
+}
---
+++ maven_jenkin/src/test/java/Sample/DemoTest.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+      @Test
+	  public void case2()
+	  {
+		  
+		  System.out.println("Print Case2");
+		  
+	  }
+	
+
+}
